87d519da48a9ef61ad4b661da4137d095270de16
shriramBaNW <72285290+shriramBaNW@users.noreply.github.com>
2024-02-08 21:48:12 -0500

    First commit

---
+++ StudyApp/StudyApp/StudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppApp.swift
+//  StudyApp
+//
+//  Created by Shriram Vasudevan on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
