a37f4e76bc12cc4f6c4a8f769c397e611e4107e1
shriramBaNW <72285290+shriramBaNW@users.noreply.github.com>
2024-02-09 23:46:08 -0500

    Done for the day

--- StudyApp/StudyApp/Views/HomePageView.swift
+++ StudyApp/StudyApp/Views/HomePageView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct HomePageView: View {
+    @State var roomCode: String = ""
+    
     var body: some View {
         NavigationStack {
             ZStack {
                 LinearGradient(colors: [.blue, .purple], startPoint: .top, endPoint: .bottom)
                     .edgesIgnoringSafeArea(.all)
-                VStack {
+                ScrollView {
                     HStack {
                         Text("Welcome Shriram")
                             .font(.largeTitle)
@@ -30,83 +32,157 @@ struct HomePageView: View {
                             .padding()
                     }
                     
-                    
-                    List {
-                        Section(header: Text("Friends").font(.title).padding(.bottom, 10)
-                            .foregroundColor(.black).listRowInsets(EdgeInsets())) {
-                            Button(action: {
-                                
-                            }, label: {
-                                HStack {
-                                    Image("Roses")
-                                        .resizable()
-                                        .frame(width: 23, height: 23)
-                                        .clipShape(Circle())
-                                        .foregroundColor(.black)
-                                        .padding(.leading)
-                                    
-                                    Text("Robert")
-                                        .foregroundColor(.black)
-                                    
-                                    Spacer()
-                                    
-                                    Image(systemName: "hand.wave")
-                                        .foregroundColor(.black)
-                                        .padding(.trailing)
-                                }
-                            })
-                            .listRowInsets(EdgeInsets())
-                                
-                            Button(action: {
-                                print("hi")
-                            }, label: {
-                                HStack {
-                                    Image("Roses")
-                                        .resizable()
-                                        .frame(width: 23, height: 23)
-                                        .clipShape(Circle())
-                                        .foregroundColor(.black)
-                                        .padding(.leading)
-                                    
-                                    Text("Ally")
-                                        .foregroundColor(.black)
-                                    
-                                    Spacer()
-                                    
-                                    Image(systemName: "hand.wave")
-                                        .foregroundColor(.black)
-                                        .padding(.trailing)
-                                }
-                            })
-                            .listRowInsets(EdgeInsets())
+                    HStack {
+                        Text("Friends").font(.title).padding(.bottom, 10)
+                            .foregroundColor(.white)
+                            .padding(.leading)
                         
-                            Button(action: {
-                                
-                            }, label: {
-                                HStack {
-                                    Image("Roses")
-                                        .resizable()
-                                        .frame(width: 23, height: 23)
-                                        .clipShape(Circle())
-                                        .foregroundColor(.black)
-                                        .padding(.leading)
-                                    
-                                    Text("Shriram")
-                                        .foregroundColor(.black)
-                                    
-                                    Spacer()
-                                    
-                                    Image(systemName: "hand.wave")
-                                        .foregroundColor(.black)
-                                        .padding(.trailing)
-                                }
-                            })
-                            .listRowInsets(EdgeInsets())
-                        }
-                        
+                        Spacer()
                     }
-                    .scrollContentBackground(.hidden)
                     
+                    VStack {
+                        Button(action: {
+                            
+                        }, label: {
+                            HStack {
+                                Image("Roses")
+                                    .resizable()
+                                    .frame(width: 23, height: 23)
+                                    .clipShape(Circle())
+                                    .foregroundColor(.black)
+                       
+                                
+                                Text("Robert")
+                                    .foregroundColor(.black)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "hand.wave")
+                                    .foregroundColor(.black)
+              
+                            }
+                        })
+   
+                        Divider()
+                            .padding(.leading)
+                        Button(action: {
+                            print("hi")
+                        }, label: {
+                            HStack {
+                                Image("Roses")
+                                    .resizable()
+                                    .frame(width: 23, height: 23)
+                                    .clipShape(Circle())
+                                    .foregroundColor(.black)
+                                
+                                Text("Ally")
+                                    .foregroundColor(.black)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "hand.wave")
+                                    .foregroundColor(.black)
+                            }
+                        })
+             
+                        Divider()
+                            .padding(.leading)
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            HStack {
+                                Image("Roses")
+                                    .resizable()
+                                    .frame(width: 23, height: 23)
+                                    .clipShape(Circle())
+                                    .foregroundColor(.black)
+
+                                
+                                Text("Shriram")
+                                    .foregroundColor(.black)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "hand.wave")
+                                    .foregroundColor(.black)
+                            }
+                        })
+                   
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 13)
+                            .fill(.white)
+                    )
+                    .padding()
+                    
+                    VStack {
+                        Text("Create Room")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                            .bold()
+                        
+                        Text("Create")
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.headline)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(.green)
+                                    .shadow(radius: 5)
+                            )
+                            .padding(.bottom)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(LinearGradient(colors: [.black.opacity(0.3),.black], startPoint: .top, endPoint: .bottom))
+                    )
+                    .padding()
+                    
+                    
+                    VStack {
+                        Text("Have A Room Code?")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                            .bold()
+                        
+                        TextField("", text: $roomCode)
+                            .placeholder(when: roomCode.isEmpty, placeholder: {
+                                Text("Code").foregroundColor(.white.opacity(0.7))
+                            })
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(LinearGradient(colors: [.blue, .purple], startPoint: .leading, endPoint: .trailing))
+                            )
+                            .padding(.vertical)
+                        
+                        Text("Join")
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.headline)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(.blue)
+                                    .shadow(radius: 5)
+                            )
+                            .padding(.bottom)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(LinearGradient(colors: [.black.opacity(0.3),.black], startPoint: .top, endPoint: .bottom))
+                    )
+                    .padding()
                     Spacer()
                 }
         
@@ -117,4 +193,18 @@ struct HomePageView: View {
 
 #Preview {
     HomePageView()
+}
+
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
 }
